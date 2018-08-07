6af5c7bcfca5f80bac303015c3c9a947ae2106ae
rohitsavant <savantrohit8@gmail.com>
2018-08-07 09:36:27 +0530

    Initial Commit

---
+++ app/src/main/java/com/narmware/jainjeevan/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.narmware.jainjeevan.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.narmware.jainjeevan.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
